803f0fef19c3f8159f4485bc60408ce6e9d2d2d7
Deathrovv <aro.maru7ian@gmail.com>
2021-10-07 22:19:00 +0200

    halfway done

---
+++ react-task-tracker/src/components/Header.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = (props) => {
+  //can use destructuring for props. Not using now to learn better
+  const onClick = (e) => {
+    console.log("click");
+  };
+  return (
+    <header className="header">
+      <h1>{props.title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+//CSS in JSX
+// const headingStyle = {
+//   color: "red",
+//   backgroundColor: "black",
+// };
+
+export default Header;
